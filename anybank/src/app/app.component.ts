6ec65974bced391f19c3dd69a9c5f6f2e7b9f9ee
reotsugua <renanaugusto.ras@gmail.com>
2025-05-03 20:10:53 -0300

    banner

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "./component/banner/banner.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
